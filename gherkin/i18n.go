f2971650914b7ea2dc8c4e53fb299d13988af409
Loren Segal <lsegal@soen.ca>
2015-03-22 02:15:36 -0700

    Add documentation for i18n

--- gherkin/i18n.go
+++ gherkin/i18n.go
@@ -2,22 +2,41 @@ package gherkin
 
 type Language string
 
+// Translation represents the Gherkin syntax keywords in a single language.
 type Translation struct {
-	Feature    string
+	// Language specific term representing a feature.
+	Feature string
+
+	// Language specific term representing the feature background.
 	Background string
-	Scenario   string
-	And        string
-	Given      string
-	When       string
-	Then       string
-	Examples   string
+
+	// Language specific term representing a scenario.
+	Scenario string
+
+	// Language specific term representing the "And" step.
+	And string
+
+	// Language specific term representing the "Given" step.
+	Given string
+
+	// Language specific term representing the "When" step.
+	When string
+
+	// Language specific term representing the "Then" step.
+	Then string
+
+	// Language specific term representing a scenario outline prefix.
+	Examples string
 }
 
 const (
+	// The language code for English translations.
 	LANG_EN = Language("en")
 )
 
 var (
+	// Translations contains internationalized translations of the Gherkin
+	// syntax keywords in a variety of supported languages.
 	Translations = map[Language]Translation{
 		LANG_EN: Translation{
 			Feature:    "Feature",
